c1caa23416330e9f6373874a32f8887e3541fe74
Dan Darach <flashbanan@gmail.com>
2026-02-28 15:19:18 +0700

    L3 started

---
+++ Assets/_Project/Develop/Runtime/UI/Core/ViewsFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagement;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Assets._Project.Develop.Runtime.UI.Core
+{
+    public class ViewsFactory
+    {
+        private readonly ResourcesAssetsLoader _resourcesAssetsLoader;
+
+        private readonly Dictionary<string, string> _viewIDToResourcesPath = new Dictionary<string, string>()
+        {
+            { ViewIDs.CurrencyView, "UI/Wallet/CurrencyView" }
+        };
+
+        public ViewsFactory(ResourcesAssetsLoader resourcesAssetsLoader)
+        {
+            _resourcesAssetsLoader = resourcesAssetsLoader;
+        }
+
+        public TView Create<TView>(string viewID, Transform parent = null) where TView : MonoBehaviour, IView
+        {
+            if (_viewIDToResourcesPath.TryGetValue(viewID, out string resourcePath) == false)
+                throw new ArgumentException($"You didn't set resource path for {typeof(TView)}, searched ID: {viewID}");
+
+            GameObject prefab = _resourcesAssetsLoader.Load<GameObject>(resourcePath);
+            GameObject instance = Object.Instantiate(prefab, parent);
+            TView view = instance.GetComponent<TView>();
+
+            if (view == null)
+                throw new InvalidOperationException($"{typeof(TView)} component not found on the view instance");
+
+            return view;
+        }
+
+        public void Release<TView>(TView view) where TView : MonoBehaviour, IView
+        {
+            Object.Destroy(view.gameObject);
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/UI/Wallet/CurrencyPresenter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.Configs.Meta.Wallet;
+using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
+using Assets._Project.Develop.Runtime.UI.CommonViews;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.UI.Wallet
+{
+    public class CurrencyPresenter
+    {
+        //business logic
+        private readonly IReadonlyVariable<int> _currency;
+        private readonly CurrencyTypes _currencyType;
+        private readonly CurrencyIconsConfig _currencyIconsConfig;
+
+        //visual
+        private readonly IconTextView _view;
+
+        private IDisposable _disposable;
+
+        public CurrencyPresenter(
+            IReadonlyVariable<int> currency,
+            CurrencyTypes currencyType,
+            CurrencyIconsConfig currencyIconsConfig,
+            IconTextView view)
+        {
+            _currency = currency;
+            _currencyType = currencyType;
+            _currencyIconsConfig = currencyIconsConfig;
+            _view = view;
+        }
+
+        public void Enable()
+        {
+            UpdateValue(_currency.Value);
+            _view.SetIcon(_currencyIconsConfig.GetSpriteFor(_currencyType));
+
+            _disposable = _currency.Subscribe(OnCurrencyChanged);
+        }
+
+        public void Disable()
+        {
+            _disposable.Dispose();
+        }
+
+        private void OnCurrencyChanged(int arg1, int newValue) => UpdateValue(newValue);
+
+        private void UpdateValue(int value) => _view.SetText(value.ToString());
+    }
+}
