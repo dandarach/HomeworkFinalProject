f8b89a8fb03b3b929ed4fdde4e9c1ce12767c3ed
Dan Darach <flashbanan@gmail.com>
2026-03-03 17:12:33 +0700

    DoTween Kill added

--- Assets/_Project/Develop/Runtime/UI/Core/PopupViewBase.cs
+++ Assets/_Project/Develop/Runtime/UI/Core/PopupViewBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Assets._Project.Develop.Runtime.UI.Core
 {
@@ -8,6 +9,9 @@ namespace Assets._Project.Develop.Runtime.UI.Core
         public event Action CloseRequest;
 
         [SerializeField] private CanvasGroup _mainGroup;
+        [SerializeField] private Transform _body;
+
+        private Tween _currentAnimation;
 
         private void Awake()
         {
@@ -18,16 +22,25 @@ namespace Assets._Project.Develop.Runtime.UI.Core
 
         public void Show()
         {
+            KillCurrentAnimation();
+
             OnPreShow();
 
             // TODO: animation
             _mainGroup.alpha = 1;
+
+            _currentAnimation = _body
+                .DOScale(1, 0.5f)
+                .From(0)
+                .SetEase(Ease.OutBack);
 
             OnPostShow();
         }
 
         public void Hide()
         {
+            KillCurrentAnimation();
+
             OnPreHide();
 
             // TODO: animation
@@ -43,5 +56,13 @@ namespace Assets._Project.Develop.Runtime.UI.Core
         protected virtual void OnPreHide() { }
 
         protected virtual void OnPostHide() { }
+
+        private void OnDestroy() => KillCurrentAnimation();
+
+        private void KillCurrentAnimation()
+        {
+            if (_currentAnimation != null)
+                _currentAnimation.Kill();
+        }
     }
 }
