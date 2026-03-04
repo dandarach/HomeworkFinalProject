d967d12aaa90fa6981c0545d1ef536ab186d617a
Dan Darach <flashbanan@gmail.com>
2026-03-04 16:10:58 +0700

    LevelsMenu Popup views added

---
+++ Assets/_Project/Develop/Runtime/UI/LevelsMenuPopup/LevelTileView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using Assets._Project.Develop.Runtime.UI.Core;
+namespace Assets._Project.Develop.Runtime.UI.LevelsMenuPopup
+{
+    public class LevelTileView : MonoBehaviour, IShowableView
+    {
+        public event Action Clicked;
+
+        [SerializeField] private Image _background;
+        [SerializeField] private TMP_Text _levelNumberText;
+        [SerializeField] private Button _button;
+
+        [SerializeField] private Color _activeColor;
+        [SerializeField] private Color _completedColor;
+        [SerializeField] private Color _blockedColor;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnClick);
+        }
+
+        public void SetLevel(string level) => _levelNumberText.text = level;
+
+        public void Block() => _background.color = _blockedColor;
+        
+        public void Complete() => _background.color = _completedColor;
+        
+        public void Activate() => _background.color = _activeColor;
+
+        public Tween Show()
+        {
+            transform.DOKill();
+
+            return transform
+                .DOScale(1, 0.1f)
+                .From(0)
+                .SetUpdate(true)
+                .Play();
+        }
+
+        public Tween Hide()
+        {
+            transform.DOKill();
+
+            return DOTween.Sequence();
+        }
+
+        private void OnDestroy()
+        {
+            transform.DOKill();
+        }
+
+        private void OnClick() => Clicked?.Invoke();
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/UI/LevelsMenuPopup/LevelsMenuPopupView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets._Project.Develop.Runtime.UI.Core;
+using TMPro;
+using DG.Tweening;
+
+namespace Assets._Project.Develop.Runtime.UI.LevelsMenuPopup
+{
+    public class LevelsMenuPopupView : PopupViewBase
+    {
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private LevelTilesListView _levelsListView;
+
+        public LevelTilesListView LevelsListView => _levelsListView;
+
+        public void SetTitle(string title) => _title.text = title;
+
+        protected override void ModifyShowAnimation(Sequence animation)
+        {
+            base.ModifyShowAnimation(animation);
+
+            foreach (LevelTileView levelTileView in _levelsListView.Elements)
+            {
+                animation.Append(levelTileView.Show());
+                animation.AppendInterval(0.1f);
+            }
+        }
+    }
+}
