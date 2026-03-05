9eb3534416f51badff75a03b3a1abecaa4714b58
Dan Darach <flashbanan@gmail.com>
2026-03-05 11:43:14 +0700

    EntitiesFactory created

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory.cs
@@ -0,0 +1,27 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
+{
+    public class EntitiesFactory
+    {
+        private readonly DIContainer _container;
+
+        public EntitiesFactory(DIContainer container)
+        {
+            _container = container;
+        }
+
+        public Entity CreateTestEntity()
+        {
+            Entity entity = new Entity();
+
+            entity.AddComponent(new MoveDirection() { Value = new ReactiveVariable<Vector3>(Vector3.forward) });
+            entity.AddComponent(new MoveSpeed() { Value = new ReactiveVariable<float>(10f) });
+
+            return entity;
+        }
+    }
+}
