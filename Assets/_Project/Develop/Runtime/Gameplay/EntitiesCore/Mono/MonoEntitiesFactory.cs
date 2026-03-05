007c7f78a0ed7c85c5c9151629389df6f734cc57
Dan Darach <flashbanan@gmail.com>
2026-03-05 16:08:39 +0700

    MonoEntitiesFactory completed

--- Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Mono/MonoEntitiesFactory.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Mono/MonoEntitiesFactory.cs
@@ -1,15 +1,24 @@
-﻿using Assets._Project.Develop.Runtime.Utilities.AssetsManagement;
+﻿using System;
+using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagement;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono
 {
-    public class MonoEntitiesFactory
+    public class MonoEntitiesFactory : IInitializable, IDisposable
     {
         private readonly ResourcesAssetsLoader _resources;
 
-        public MonoEntitiesFactory(ResourcesAssetsLoader resources)
+        private readonly EntitiesLifeContext _entitiesLifeContext;
+
+        private readonly Dictionary<Entity, MonoEntity> _entityToMono = new();
+
+        public MonoEntitiesFactory(ResourcesAssetsLoader resources, EntitiesLifeContext entitiesLifeContext)
         {
             _resources = resources;
+            _entitiesLifeContext = entitiesLifeContext;
         }
 
         public MonoEntity Create(Entity entity, Vector3 position, string path)
@@ -20,7 +29,38 @@ namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono
 
             viewInstance.Setup(entity);
 
+            _entityToMono.Add(entity, viewInstance);
+
             return viewInstance;
+        }
+        
+        public void Initialize()
+        {
+            _entitiesLifeContext.Released += OnEntityReleased;
+        }
+
+        public void Dispose()
+        {
+            _entitiesLifeContext.Released -= OnEntityReleased;
+
+            foreach (Entity entity in _entityToMono.Keys)
+                CleanupFor(entity);
+
+            _entityToMono.Clear();
+        }
+
+        private void OnEntityReleased(Entity entity)
+        {
+            CleanupFor(entity);
+
+            _entityToMono.Remove(entity);
+        }
+
+        private void CleanupFor(Entity entity)
+        {
+            MonoEntity monoEntity = _entityToMono[entity];
+            monoEntity.Cleanup(entity);
+            Object.Destroy(monoEntity.gameObject);
         }
     }
 }
