ff41431b24c2e1acd29e9ada3726968c2fa94450
Dan Darach <flashbanan@gmail.com>
2026-03-16 13:05:23 +0700

    L3 Homework continue

---
+++ Assets/_Project/Develop/Runtime/UI/Gameplay/GameplayScreenPresenter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Meta.Features.Stats;
+using Assets._Project.Develop.Runtime.UI.Core;
+
+namespace Assets._Project.Develop.Runtime.UI.Gameplay
+{
+    public class GameplayScreenPresenter : IPresenter
+    {
+        private readonly GameplayScreenView _screen;
+        private readonly ProjectPresentersFactory _projectPresentersFactory;
+        private readonly StatsResetService _statsResetService;
+        private readonly List<IPresenter> _childPresenters = new();
+
+        public GameplayScreenPresenter(
+            GameplayScreenView screen,
+            ProjectPresentersFactory projectPresentersFactory,
+            StatsResetService statsResetService)
+        {
+            _screen = screen;
+            _projectPresentersFactory = projectPresentersFactory;
+            _statsResetService = statsResetService;
+        }
+
+        public void Initialize()
+        {
+            foreach (IPresenter presenter in _childPresenters)
+                presenter.Initialize();
+        }
+
+        public void Dispose()
+        {
+            foreach (IPresenter presenter in _childPresenters)
+                presenter.Dispose();
+
+            _childPresenters.Clear();
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/UI/Gameplay/GameplayScreenView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.UI.CommonViews;
+using Assets._Project.Develop.Runtime.UI.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets._Project.Develop.Runtime.UI.Gameplay
+{
+    public class GameplayScreenView : MonoBehaviour, IView
+    {
+        public event Action OpenLevelsMenuButtonClicked;
+        public event Action ResetStatsButtonClicked;
+
+        [field: SerializeField] public IconTextListView WalletView { get; private set; }
+        [field: SerializeField] public IconTextListView StatsView { get; private set; }
+
+        [SerializeField] private Button _openLevelsMenuButton;
+        [SerializeField] private Button _resetStatsButton;
+
+        private void OnEnable()
+        {
+            _openLevelsMenuButton.onClick.AddListener(OnOpenLevelsMenuButtonClicked);
+            _resetStatsButton.onClick.AddListener(OnResetStatsButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _openLevelsMenuButton.onClick.RemoveListener(OnOpenLevelsMenuButtonClicked);
+            _resetStatsButton.onClick.RemoveListener(OnResetStatsButtonClicked);
+        }
+
+        private void OnOpenLevelsMenuButtonClicked() => OpenLevelsMenuButtonClicked?.Invoke();
+
+        private void OnResetStatsButtonClicked() => ResetStatsButtonClicked?.Invoke();
+    }
+}
