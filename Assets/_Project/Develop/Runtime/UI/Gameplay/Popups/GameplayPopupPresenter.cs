79231aff653705fbdbe85e83726006c4e3fcc80a
Dan Darach <flashbanan@gmail.com>
2026-03-17 17:43:21 +0700

    GameplayPopupService started

---
+++ Assets/_Project/Develop/Runtime/UI/Gameplay/Popups/GameplayPopupPresenter.cs
@@ -0,0 +1,50 @@
+﻿using Assets._Project.Develop.Runtime.UI.Core;
+using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagement;
+
+namespace Assets._Project.Develop.Runtime.UI.Gameplay.Popups
+{
+    public class GameplayPopupPresenter : PopupPresenterBase
+    {
+        private const string TitleName = "LEVELS";
+
+        private readonly ProjectPresentersFactory _presentersFactory;
+        private readonly ViewsFactory _viewsFactory;
+        private readonly GameplayPopupView _view;
+
+        public GameplayPopupPresenter(
+            ICoroutinesPerformer coroutinesPerformer,
+            ProjectPresentersFactory presentersFactory,
+            ViewsFactory viewsFactory,
+            GameplayPopupView view) : base(coroutinesPerformer)
+        {
+            _presentersFactory = presentersFactory;
+            _viewsFactory = viewsFactory;
+            _view = view;
+        }
+
+        protected override PopupViewBase PopupView => _view;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _view.SetTitle(TitleName);
+
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        protected override void OnPreShow()
+        {
+            base.OnPreShow();
+        }
+
+        protected override void OnPreHide()
+        {
+            base.OnPreHide();
+        }
+    }
+}
