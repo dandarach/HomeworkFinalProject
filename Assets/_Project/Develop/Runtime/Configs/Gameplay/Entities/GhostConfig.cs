fca3e3eb2d4f1fecdde813c38957ecb0acf0aa11
Dan Darach <flashbanan@gmail.com>
2026-04-04 17:45:48 +0700

    Entities configs created

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/GhostConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewGhostConfig", fileName = "GhostConfig")]
+
+    public abstract class GhostConfig : EntityConfig
+    {
+        [field: SerializeField] public string PrefabPath { get; private set; } = "Entities/Ghost";
+        [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 9f;
+        [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 900f;
+        [field: SerializeField, Min(0)] public float MaxHealth { get; private set; } = 100f;
+        [field: SerializeField, Min(0)] public float BodyContactDamage { get; private set; } = 50f;
+        [field: SerializeField, Min(0)] public float DeathProcessTime { get; private set; } = 2f;
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/HeroConfig.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewHeroConfig", fileName = "HeroConfig")]
+
+    public abstract class HeroConfig : EntityConfig
+    {
+        [field: SerializeField] public string PrefabPath { get; private set; } = "Entities/Hero";
+        [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 9f;
+        [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 900f;
+        [field: SerializeField, Min(0)] public float AttackProcessTime { get; private set; } = 1.5f;
+        [field: SerializeField, Min(0)] public float AttackDelayTime { get; private set; } = 0.75f;
+        [field: SerializeField, Min(0)] public float AttackCooldown { get; private set; } = 1f;
+        [field: SerializeField, Min(0)] public float InstantAttackDamage { get; private set; } = 50f;
+        [field: SerializeField, Min(0)] public float MaxHealth { get; private set; } = 100f;
+        [field: SerializeField, Min(0)] public float DeathProcessTime { get; private set; } = 2f;
+    }
+}
