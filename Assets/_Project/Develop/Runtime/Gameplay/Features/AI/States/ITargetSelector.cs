866d5de08654d86ec8a6738096f37d10de5891ef
Dan Darach <flashbanan@gmail.com>
2026-04-04 16:08:46 +0700

    EntityAPIGenerator - TryGet method generation added

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/AI/States/ITargetSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.AI.States
+{
+    public interface ITargetSelector
+    {
+        Entity SelectTargetFrom(IEnumerable<Entity> targets);
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/AI/States/NearestDamageableTargetSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.AI.States
+{
+    public class NearestDamageableTargetSelector : ITargetSelector
+    {
+        private Entity _source;
+        private Transform _sourceTransform;
+
+        public NearestDamageableTargetSelector(Entity entity)
+        {
+            _source = entity;
+            _sourceTransform = entity.Transform;
+        }
+
+        public Entity SelectTargetFrom(IEnumerable<Entity> targets)
+        {
+            IEnumerable<Entity> selectedTragets = targets.Where(target =>
+            {
+                return false;
+            });
+
+            return null;
+        }
+    }
+}
