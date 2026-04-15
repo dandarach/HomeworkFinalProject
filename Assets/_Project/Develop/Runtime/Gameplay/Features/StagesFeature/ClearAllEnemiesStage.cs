e820b5e68bdaf5e14a5e2582ddda7e7c6dd6aa52
Dan Darach <flashbanan@gmail.com>
2026-04-15 15:37:45 +0700

    GameStages started

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/StagesFeature/ClearAllEnemiesStage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.Configs.Gameplay.Stages;
+using Assets._Project.Develop.Runtime.Gameplay.Features.Enemies;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.StagesFeature
+{
+    public class ClearAllEnemiesStage : IStage
+    {
+        private ClearAllEnemiesStageConfig _config;
+        private ReactiveEvent _completed = new();
+        private EnemiesFactory _enemiesFactory;
+        private bool _inProcess;
+
+        public ClearAllEnemiesStage(
+            ClearAllEnemiesStageConfig config,
+            EnemiesFactory enemiesFactory)
+        {
+            _config = config;
+            _enemiesFactory = enemiesFactory;
+        }
+
+        public IReadonlyEvent Completed => _completed;
+
+        public void Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Start()
+        {
+            if (_inProcess)
+                throw new InvalidOperationException("Game mode already started");
+
+            SpawnEnemies();
+
+            _inProcess = true;
+        }
+
+        public void Update(float deltaTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SpawnEnemies()
+        {
+            foreach (EnemyItemConfig enemyItemConfig in _config.EnemyItems)
+                SpawnEnemy(enemyItemConfig);
+        }
+
+        private void SpawnEnemy(EnemyItemConfig enemyItemConfig)
+        {
+            _enemiesFactory.Create(enemyItemConfig.SpawnPosition, enemyItemConfig.EnemyConfig);
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/StagesFeature/IStage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.StagesFeature
+{
+    public interface IStage : IDisposable
+    {
+        IReadonlyEvent Completed { get; }
+
+        void Start();
+        void Update(float deltaTime);
+        void Cleanup();
+    }
+}
