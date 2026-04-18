083144f5308c06abaa770e8e410146d3f9267825
Dan Darach <flashbanan@gmail.com>
2026-04-18 13:41:46 +0700

    InstantAttackAnimationSpeedView added
    Calculates attack animation speed automatically, syncs attack process and animation

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Attack/InstantAttackAnimationSpeedView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Attack
+{
+    [RequireComponent(typeof(Animator))]
+    public class InstantAttackAnimationSpeedView : EntityView
+    {
+        private readonly int AttackAnimationSpeedMultiplierKey = Animator.StringToHash("AttackAnimationSpeedMultiplier");
+
+        [SerializeField] private AnimationClip _animationClip;
+        [SerializeField] private Animator _animator;
+
+        private IReadonlyVariable<float> _attackProcessTime;
+
+        private void OnValidate()
+        {
+            _animator ??= GetComponent<Animator>();
+        }
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _attackProcessTime = entity.AttackProcessInitialTime;
+            _animator.SetFloat(AttackAnimationSpeedMultiplierKey, _animationClip.length / _attackProcessTime.Value);
+        }
+    }
+}
